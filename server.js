9f4a39838d8d70cdc9aaad944a3b1400c50fe645
brandongatlin <brandongatlin1981@me.com>
2020-07-26 20:32:06 -0500

    updated server

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const cors = require('cors');
-
+const http = require('http');
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 const db = require('./models');
-
 // Creating express app and configuring middleware needed for authentication
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 app.use(cors());
 // We need to use sessions to keep track of our user's login status
 app.use(
@@ -18,18 +17,18 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-
-// app.get('/', function(req, res) {
-//     res.send('Group 1 API');
-// });
-
+app.use(express.static(__dirname + '/dist'));
 require('./routes/api-routes.js')(app);
 require('./routes/post-api-routes.js')(app);
-app.use(express.static('dist'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/client/index.html'));
+});
+app.set('port', PORT);
+const server = http.createServer(app);
 db.sequelize.sync().then(function() {
-    app.listen(PORT, function() {
+    server.listenn(PORT, function() {
         console.log(
-            '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
+            '==> :earth_americas:  Listening on port %s. Visit http://localhost:%s/ in your browser.',
             PORT,
             PORT
         );
